65d5e7e709ba567c586238a20acd1d8b9df8887a
kevd82 <80932709+kevd82@users.noreply.github.com>
2024-03-19 15:16:59 -0700

    context work continues

--- frontend/src/Pages/ShopCategory.js
+++ frontend/src/Pages/ShopCategory.js
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
 import "./CSS/ShopCategory.css";
 import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
 
 const ShopCategory = (props) => {
     const {all_product} = useContext(ShopContext);
   return (
     <div className="shop-category">
+      <div className="shopcategory-indexSort">
+        <p><span>Showing 1-12</span> out of 36 products</p>
+      </div>
+        <div className="shopcaegory-sort">
+          Sort by <img src={dropdown_icon} alt=""/>
+        </div>
 
     </div>
   )
