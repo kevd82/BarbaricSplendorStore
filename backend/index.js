c2591aa750294887e1c147ca5ac286fac9643ec0
kevd82 <80932709+kevd82@users.noreply.github.com>
2024-04-02 22:29:30 -0700

    upload to db endpoint work

--- backend/index.js
+++ backend/index.js
@@ -65,6 +65,34 @@ const Product = mongoose.model("Product", {
     }
 })
 
+app.post("/addproduct", async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;
+    }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        price:req.body.price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
 app.listen(port,(error)=>{
     if (!error){
         console.log("You have conquered port "+port+"!")
