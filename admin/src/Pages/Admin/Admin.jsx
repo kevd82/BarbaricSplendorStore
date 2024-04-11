67dca8d04f159df4360833f00fe6f0e91ed60a89
kevd82 <80932709+kevd82@users.noreply.github.com>
2024-04-10 20:25:28 -0700

    addproduct and listproduct admin work

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import "./Admin.css";
+import {Routes, Route} from "react-router-dom";
 import Sidebar from '../../Components/Sidebar/Sidebar';
+import ListProduct from '../../Components/ListProduct/ListProduct';
+import AddProduct from '../../Components/AddProduct/AddProduct';
 
 
 const Admin = () => {
   return (
     <div className="admin">
         <Sidebar/>
+        <Routes>
+          <Route path="/addproduct" element={<AddProduct/>}/>
+          <Route path="/listproduct" element={<ListProduct/>}/>
+        </Routes>
     </div>
   )
 }
