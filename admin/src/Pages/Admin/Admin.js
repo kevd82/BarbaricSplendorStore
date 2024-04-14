4ef0cf5e84c73026e9fa7eccd2289721f5762025
kevd82 <80932709+kevd82@users.noreply.github.com>
2024-04-13 23:33:55 -0700

    restarted admin panel

--- admin/src/Pages/Admin/Admin.js
+++ admin/src/Pages/Admin/Admin.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./Admin.css";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import {Routes, Route} from "react-router-dom";
 
 const Admin = () => {
   return (
