d676734d7401712ca3b4ac19408288ac19b8c28f
kevd82 <80932709+kevd82@users.noreply.github.com>
2024-04-20 02:12:53 -0700

    more ListProduct .css

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -20,22 +20,25 @@ const ListProduct = () => {
     <div className="list-product">
       <h1>All Products</h1>
       <div className="listproduct-format-main">
-        <p>Products</p>
-        <p>Title</p>
+        <p className="listproduct-product-image">Product Image</p>
+        <p className="listproduct-product-title">Title</p>
         <p>Price</p>
         <p>Category</p>
         <p>Remove</p>
+        <p>Update</p>
       </div>
       <div className="listproduct-allproducts">
         <hr />
         {allproducts.map((product,index)=>{
-          return <div key={index} className="listproduct-format-main listproduct-format">
+          return <><div key={index} className="listproduct-format-main listproduct-format">
             <img src={product.image} alt="" className="listproduct-product-icon"/>
             <p>{product.title}</p>
             <p>${product.price}</p>
             <p>{product.category}</p>
             <img className="listproduct-remove-icon" src={cross_icon} alt="" />
           </div>
+          <hr />
+          </>
         })}
       </div>
     </div>
