81683862f58e05d84eae1d63ed06fd160e1b3cef
Your Name <barbaricsplendor@gmail.com>
2024-05-06 20:36:28 -0700

    finished add to cart functionality

--- backend/index.js
+++ backend/index.js
@@ -154,7 +154,9 @@ const fetchUser= async(req,res,next)=>{
 app.post("/addtocart", fetchUser, async (req,res)=>{
 
     let userData = await Users.findOne({_id:req.user.id});
-
+    userData.cartData[req.body.itemId] +=1;
+    await Users.findOneAndUpdate({_id:req.user.id}, {cartData:userData.cartData});
+    res.send("Added")
 })
 
 const Users = mongoose.model("Users" ,{
