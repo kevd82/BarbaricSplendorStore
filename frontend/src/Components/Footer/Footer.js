772ab488347ba82ed361e1f76c76028bbb718290
Your Name <barbaricsplendor@gmail.com>
2024-05-16 00:09:08 -0700

    css on footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,33 +1,18 @@
 import React from 'react'
 import "./Footer.css";
-import footer_logo from "../Assets/logo_footer.png";
-import instagram_icon from "../Assets/instagram_icon.png";
-import facebook_icon from "../Assets/facebook_icon.png"; 
 
 const Footer = () => {
 
     return (
         <div className="footer">
-            <div className="footer-logo">
-                <img src={footer_logo} alt="" />       
-            </div>  
-            
-        
-            <div className="footer-social-icons">
-                
-                    <img src={instagram_icon} alt=""/>
-                    <img src={facebook_icon} alt=""/>
-                <div/>
                 <div className="footer-copyright">
+                    <p>Logo and clipart by Gilead.</p>
                     <p>Copyright @ 2024 - All Rights Reserved.</p>
-                    <p>Logo by Gilead.</p>
-                    <p>Clipart by Gilead.</p>
+                    <p>"Game Savage" logos by Igor Mugerza.</p>
                 </div>
-                <div className="footer-logo">
-                <img src={footer_logo} alt="" />       
-                </div>  
+                
             </div>
-        </div>
+    
     )
 }
 
--- frontend/src/Pages/ShopCategory.js
+++ frontend/src/Pages/ShopCategory.js
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from 'react';
 import "./CSS/ShopCategory.css";
 import { ShopContext } from '../Context/ShopContext';
-import dropdown_icon from "../Components/Assets/dropdown_icon.png";
 import Item from "../Components/Item/Item";
 
 const ShopCategory = (props) => {
